a53e08b30032d2af765f768c16fa057a0941589c
Jonnii Ringo <jonathansamuels@Jonathans-MBP-2.lan>
2024-12-31 16:55:52 -0800

    Simplifying the code by using the spread operator

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -13,28 +13,12 @@ function App() {
     setContact(prevValue => {
       if (name === "fName"){
         return {
-          fName: value,
-          lName: prevValue.lName,
-          email: prevValue.email
+          ... prevValue,
+          [name]: value,
         };
-      } else if(name === "lName"){
-        return {
-          fName:prevValue.fName,
-          lName: value,
-          email: prevValue.email
-        };
-      } else if (name === "email"){
-        return {
-          fName: prevValue.fName,
-          lName: prevValue.lName,
-          email: value
-        }; 
       }
     });
   }
-
-
-
 
 
   return (
